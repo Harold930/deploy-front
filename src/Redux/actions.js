86c9ee458a007eaf415add874e5bf4ebee6b2806
Harold Rodriguez <harol20082008@hotmail.com>
2021-10-28 03:33:51 -0300

    arreglos ultimos

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,4 +1,6 @@
-import axios, { Axios } from 'axios';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 //------------------------GETS------------------------------------------------
 
